5b3a152f3d55f74b080a539e62b9753550206d4f
Dan Rosen <drosen@precipart.com>
2020-05-21 17:38:37 -0400

    new todo component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import Todo from './Todo'
+import NewTodoForm from './NewTodoForm'
 
 class TodoList extends Component {
     constructor(props) {
@@ -9,6 +10,14 @@ class TodoList extends Component {
         this.state = {
             todos: [{ task: 'Walk the fish' }, { task: 'Groom chickens' }]
         }
+
+        this.create = this.create.bind(this)
+    }
+
+    create(newTodo) {
+        this.setState({
+            todos: [...this.state.todos, newTodo]
+        })
     }
 
     render() {
@@ -16,6 +25,7 @@ class TodoList extends Component {
         return (
             <div>
                 <h1>Todo List!</h1>
+                <NewTodoForm createTodo={this.create} />
                 <ul>{todos}</ul>
             </div>
         )
